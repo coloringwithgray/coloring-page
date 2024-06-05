1dffb15d05e991124f2c9cd60ddd291a760fe345
coloringwithgray <kayron@houseofcalloway.com>
2024-06-05 13:06:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,19 @@ const hiddenText = document.querySelector('.hidden-text');
 function resizeCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    ctx.fillStyle = 'gray';
+    ctx.fillStyle = 'white';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function isCanvasColored() {
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+    for (let i = 0; i < data.length; i += 4) {
+        if (data[i] !== 128 || data[i + 1] !== 128 || data[i + 2] !== 128) {
+            return false;
+        }
+    }
+    return true;
 }
 
 resizeCanvas();
@@ -16,12 +27,17 @@ canvas.addEventListener('mousemove', (e) => {
     const y = e.clientY;
     const radius = 50;
 
-    ctx.globalCompositeOperation = 'destination-out';
+    ctx.globalCompositeOperation = 'source-over';
+    ctx.fillStyle = 'gray';
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, Math.PI * 2);
     ctx.fill();
     ctx.closePath();
-    ctx.globalCompositeOperation = 'source-over';
+
+    if (isCanvasColored()) {
+        hiddenText.style.display = 'block';
+    }
 });
 
 window.addEventListener('resize', resizeCanvas);
+hiddenText.style.display = 'none';
