23051a4d69099fe8c15d3c9eb8748b8b823060c6
coloringwithgray <kayron@houseofcalloway.com>
2024-06-05 21:45:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function initializeCanvas() {
 function drawLine(x, y, lastX, lastY) {
     ctx.globalCompositeOperation = 'source-over';
     ctx.strokeStyle = 'gray';
-    ctx.lineWidth = 15; // Thicker drawing line
+    ctx.lineWidth = 15;
     ctx.lineCap = 'round';
     ctx.beginPath();
     ctx.moveTo(lastX, lastY);
@@ -46,7 +46,6 @@ function handlePointerMove(e) {
 
 function handlePointerUp() {
     isDrawing = false;
-    crayon.style.display = 'none';
     checkCanvasColored();
 }
 
@@ -75,8 +74,8 @@ function getPointerPosition(e) {
 }
 
 function moveCrayon(x, y) {
-    crayon.style.left = `${x - 15}px`; // Adjusted to be closer to the cursor
-    crayon.style.top = `${y - 50}px`;  // Adjusted to be closer to the cursor
+    crayon.style.left = `${x - 15}px`;
+    crayon.style.top = `${y - 50}px`;
 }
 
 canvas.addEventListener('mousedown', handlePointerDown);
